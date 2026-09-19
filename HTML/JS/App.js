7f481f563ba32ec29f56d34158148eeae375bff7
kevin ramirez <kevinramirezm0298@gmail.com>
2026-09-18 23:00:12 -0500

    primera invitacion de boda

---
+++ HTML/JS/App.js
@@ -0,0 +1,41 @@
+const fechaEvento = new Date("2026-12-15T18:00:00").getTime();
+
+function actualizarContador() {
+
+    const ahora = new Date().getTime();
+
+    const diferencia = fechaEvento - ahora;
+
+    if (diferencia <= 0) {
+        document.getElementById("dias").textContent = "0";
+        document.getElementById("horas").textContent = "0";
+        document.getElementById("minutos").textContent = "0";
+        document.getElementById("segundos").textContent = "0";
+        return;
+    }
+
+    const dias = Math.floor(
+        diferencia / (1000 * 60 * 60 * 24)
+    );
+
+    const horas = Math.floor(
+        (diferencia / (1000 * 60 * 60)) % 24
+    );
+
+    const minutos = Math.floor(
+        (diferencia / (1000 * 60)) % 60
+    );
+
+    const segundos = Math.floor(
+        (diferencia / 1000) % 60
+    );
+
+    document.getElementById("dias").textContent = dias;
+    document.getElementById("horas").textContent = horas;
+    document.getElementById("minutos").textContent = minutos;
+    document.getElementById("segundos").textContent = segundos;
+}
+
+actualizarContador();
+
+setInterval(actualizarContador, 1000);
